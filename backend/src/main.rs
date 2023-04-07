b6c33ff34fd23cd8ba6c1f5fc6d89023449d1cd6
kumavale <kumavale.hyperion@gmail.com>
2023-04-07 19:18:21 +0900

    `sqlx`を使用して`sqlite`からデータを持ってくるようにした

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,53 +1,62 @@
+use std::sync::Arc;
+
 use axum::{
+    Extension,
     http::HeaderValue,
     routing::get,
     Router,
 };
 use http::{Method};
 use serde::{Deserialize, Serialize};
+use sqlx::{migrate::MigrateDatabase, Sqlite, SqlitePool};
 use tower_http::cors::CorsLayer;
 
-#[derive(Serialize, Deserialize)]
-struct Message {
-    text: String,
+const DB_URL: &str = "sqlite://todos.db";
+
+#[derive(sqlx::FromRow)]
+#[derive(Debug, Serialize, Deserialize)]
+struct Todos {
+    id: String,
+    content: String,
+    done: bool,
 }
 
-async fn hello() -> String {
-r#"[
-  {
-    "id": 1,
-    "content": "Create react appをインストールする",
-    "done": true
-  },
-  {
-    "id": 2,
-    "content": "JSON Server仮のAPIを作成する",
-    "done": false
-  },
-  {
-    "id": 3,
-    "content": "Chakra UIをインストールする",
-    "done": false
-  }
-]"#.into()
+async fn get_all_todos_data(
+    Extension(db): Extension<Arc<SqlitePool>>,
+) -> String {
+    let mut conn = db.acquire().await.unwrap();
+    let todos = sqlx::query_as::<_, Todos>("SELECT * FROM todos;")
+        .fetch_all(&mut conn)
+        .await
+        .unwrap();
+    serde_json::to_string(&todos).unwrap()
 }
-
-//async fn post_message(msg: serde_json::Json<Message>) -> String {
-//    let message = msg.into_inner();
-//    format!("Received message: {}", message.text)
-//}
 
 #[tokio::main]
 async fn main() {
+    // DBの作成
+    if !Sqlite::database_exists(DB_URL).await.unwrap_or(false) {
+        println!("Creating database {}", DB_URL);
+        match Sqlite::create_database(DB_URL).await {
+            Ok(_) => println!("Create db success"),
+            Err(error) => panic!("error: {}", error),
+        }
+    }
+    // テーブルの作成
+    let pool = SqlitePool::connect(DB_URL).await.unwrap();
+    sqlx::query(r#"CREATE TABLE IF NOT EXISTS todos (id TEXT PRIMARY KEY, content TEXT, done INTEGER);"#)
+        .execute(&pool)
+        .await
+        .unwrap();
+
     let app = Router::new()
-        .route("/todos", get(hello))
-        //.route("/messages", post(post_message));
+        .route("/todos", get(get_all_todos_data))
+        .layer(Extension(Arc::new(pool)))
         .layer(
             CorsLayer::new()
                 .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
                 .allow_methods([Method::GET])
-        )
-        ;
+        );
 
     eprintln!("listening on localhost:3100");
     axum::Server::bind(&"127.0.0.1:3100".parse().unwrap())
